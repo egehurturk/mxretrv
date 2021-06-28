198e8ee470c18392c95a8a627ba5b27ad9efb95f
Ege Hurturk <ege.hurturk@gmail.com>
2021-06-28 11:49:17 +0300

    editorconfig

--- src/main/java/org/mxretrv/App.java
+++ src/main/java/org/mxretrv/App.java
@@ -1,8 +1,7 @@
 package org.mxretrv;
 
 public class App {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Woooo" );
     }
 }
