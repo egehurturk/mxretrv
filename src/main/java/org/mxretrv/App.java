caefe502d358b19b477874d22906719a27c283ce
Ege Hurturk <ege.hurturk@gmail.com>
2021-06-28 12:43:07 +0300

    Need to have argument checking

--- src/main/java/org/mxretrv/App.java
+++ src/main/java/org/mxretrv/App.java
@@ -1,12 +1,31 @@
 package org.mxretrv;
 
+
 public class App {
+    /** List of domain names (passed into args) */
+    private static String[] domainNames;
+    /** input file ('\n' separated file) containing domain names */
+    private static String inputFileStr;
+    /** output file (',' separated file) containing MX records */
+    private static String outputFileStr;
+    /** number of domains to be processed in one thread */
+    private static int batchSize;
+
+    private static boolean readFromStdin;
+    private static boolean multiThreadingEnabled;
+    private static boolean verboseModeEnabled;
+
+
     public static void main( String[] args ) {
-        if (args.length == 0 ) {
-            System.err.println( "Usage: MXLookup host [...]" );
-            System.exit( 99 );
+        if (args.length == 1) { // read from stdin
+            readFromStdin = true;
         }
+
 
     }
 
+    private static void getOptions() {
+
+
+    }
 }
