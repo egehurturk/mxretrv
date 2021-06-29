6caa6b6479ecab53d52633cc928c0690cf6e485a
Ege Hurturk <ege.hurturk@gmail.com>
2021-06-29 16:24:01 +0300

    Completed argument processing, everything works fine.

--- src/main/java/org/mxretrv/App.java
+++ src/main/java/org/mxretrv/App.java
@@ -1,9 +1,10 @@
 package org.mxretrv;
 
+import java.util.ArrayList;
 
 public class App {
     /** List of domain names (passed into args) */
-    private static String[] domainNames;
+    private static ArrayList<String> domains = new ArrayList<>();
     /** input file ('\n' separated file) containing domain names */
     private static String inputFileStr;
     /** output file (',' separated file) containing MX records */
@@ -11,21 +12,43 @@ public class App {
     /** number of domains to be processed in one thread */
     private static int batchSize;
 
-    private static boolean readFromStdin;
     private static boolean multiThreadingEnabled;
     private static boolean verboseModeEnabled;
+    private static boolean useStdout;
+
+    public static final int DEFAULT_BATCH_SIZE = 1;
 
 
     public static void main( String[] args ) {
-        if (args.length == 1) { // read from stdin
-            readFromStdin = true;
-        }
-
-
+        setCommandLineOptions(args);
     }
 
-    private static void getOptions() {
+    private static void setCommandLineOptions(String[] args) {
+        ArgumentParser parser = new ArgumentParser(args);
+        inputFileStr = parser.getInputArgument() == null ? "" : parser.getInputArgument() ;
+        outputFileStr = parser.getOutputArgument() == null ? "" : parser.getOutputArgument();
+        useStdout = outputFileStr.equals("");
+        multiThreadingEnabled = parser.getMultiArgument();
+        verboseModeEnabled = parser.getVerboseArgument();
 
+        if (parser.getBatchArgument() == null && multiThreadingEnabled)
+            // if multi-threading is enabled and batch size argument is missing, set it to default value
+            batchSize = DEFAULT_BATCH_SIZE;
+        else if (!multiThreadingEnabled || parser.getBatchArgument() == null)
+            // if multi-threading is not enabled or batch size argument is missing, ignore it
+            batchSize = 0;
+        else
+            batchSize = Integer.parseInt(parser.getBatchArgument());
+    }
 
+    private static void printOptions() {
+        System.out.println(inputFileStr);
+        System.out.println(outputFileStr);
+        System.out.println(batchSize);
+        System.out.println(multiThreadingEnabled);
+        System.out.println(verboseModeEnabled);
+        System.out.println(useStdout);
     }
 }
+
+//
---
+++ src/main/java/org/mxretrv/ArgumentParser.java
@@ -0,0 +1,76 @@
+package org.mxretrv;
+
+import org.apache.commons.cli.*;
+
+public class ArgumentParser {
+    private final String[] args;
+    private final CommandLine cmd;
+
+    public ArgumentParser(String[] args) {
+        this.args = args;
+        Options options = prepareOptions();
+
+        HelpFormatter formatter = new HelpFormatter();
+        customizeHelpFormatter(formatter);
+
+        CommandLineParser parser = new DefaultParser();
+        cmd = parseCommandLine(parser, options, formatter);
+    }
+
+    private Options prepareOptions() {
+        Options options = new Options();
+        Option input = new Option("i", "input", true, "a text file containing domain names delimited with \"\\n\"");
+        input.setRequired(true);
+        input.setArgName("input_file");
+        options.addOption(input);
+
+        Option output = new Option("o", "output", true, "CSV file to output MX records for each domain name (otherwise stdout is used)");
+        output.setRequired(false);
+        output.setArgName("output_file");
+        options.addOption(output);
+
+        Option batch_size = new Option("b", "batch-size", true, "the number of domain names to process in each thread (if multithreading is not enabled, the value of this argument will be ignored)");
+        batch_size.setRequired(false);
+        batch_size.setArgName("size");
+        options.addOption(batch_size);
+
+        Option multi = new Option("m", "multi", false, "enable multithreading");
+        multi.setRequired(false);
+        options.addOption(multi);
+
+        Option verbose = new Option("v", "verbose", false, "output extra information about the process");
+        verbose.setRequired(false);
+        options.addOption(verbose);
+        return options;
+    }
+
+    private void customizeHelpFormatter(HelpFormatter help) {
+        help.setOptionComparator(null);
+        help.setWidth(140);
+        help.setNewLine("\n\n");
+    }
+
+    private CommandLine parseCommandLine(CommandLineParser parser, Options options, HelpFormatter formatter) {
+        CommandLine cmd = null;
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            String header = "Save DNS MX records of domain names\n\n";
+            String footer = "\nMade by Ege Hurturk";
+            formatter.printHelp("mxretrv", header, options, footer, true);
+            System.exit(1);
+        }
+        return cmd;
+    }
+
+    public String[] getArgs() { return this.args; }
+
+    public String getInputArgument() { return cmd.getOptionValue("input"); }
+    public String getOutputArgument() { return cmd.getOptionValue("output"); }
+    public String getBatchArgument() { return cmd.getOptionValue("batch-size"); }
+    public boolean getMultiArgument() { return cmd.hasOption("multi"); }
+    public boolean getVerboseArgument() { return cmd.hasOption("verbose"); }
+
+
+}
