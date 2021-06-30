be8ef44eb770f20ae9bea8e515323f514ffb0910
Ege Hurturk <ege.hurturk@gmail.com>
2021-06-30 17:30:03 +0300

    Added logging

--- src/main/java/org/mxretrv/App.java
+++ src/main/java/org/mxretrv/App.java
@@ -1,6 +1,12 @@
 package org.mxretrv;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.mxretrv.threads.ThreadOptimizer;
+import org.mxretrv.utils.ArgumentParser;
+
 public class App {
+    protected static Logger logger = LogManager.getLogger(App.class);
     /** input file ('\n' separated file) containing domain names */
     private static String inputFileStr;
     private static long N;
@@ -25,9 +31,14 @@ public class App {
 
     public static void main( String[] args ) {
         setCommandLineOptions(args);
-        printOptions();
+        logOptions();
+
     }
 
+    /**
+     * Set parameters from command line.
+     * @param args argument vector
+     */
     private static void setCommandLineOptions(String[] args) {
         ArgumentParser parser = new ArgumentParser(args);
         inputFileStr = parser.getInputArgument() ;
@@ -40,9 +51,11 @@ public class App {
 
         if (multiThreadingEnabled) {
             batchSize = computeBatchSize(N, parser);
+            // remSize = n mod batchSize
+            // a = nq + r, n = nThreads, q = batchSize, r = remainder
             nThreads =  N / batchSize;
             remSize = N - batchSize * nThreads;
-            remThreads = (int) Math.ceil( ((float) remSize) / batchSize );
+            remThreads = (int) Math.ceil( ((float) remSize) / batchSize ); // always = 1
         } else {
             batchSize = N;
             nThreads = 1;
@@ -50,10 +63,16 @@ public class App {
         }
     }
 
+    /**
+     * Compute the ideal batch size, given the size of the input
+     * @param N size of input
+     * @param parser argument parser to check existance of batch option
+     * @return value of batch size
+     */
     private static long computeBatchSize(long N, ArgumentParser parser) {
         long b_t;
         DEFAULT_BATCH_SIZE = ThreadOptimizer.computeBatchSize(N);
-        if (N <= 40_000)
+        if (N <= 30_000)
             DEFAULT_BATCH_SIZE = 5000;
 
         if (parser.getBatchArgument() == null && multiThreadingEnabled)
@@ -65,18 +84,22 @@ public class App {
         return Math.min(b_t, N);
     }
 
-    private static void printOptions() {
-        System.out.println("input file: " + inputFileStr);
-        System.out.println("output file: " + outputFileStr);
-        System.out.println("batch size: " + batchSize);
-        System.out.println("multi? " + multiThreadingEnabled);
-        System.out.println("verbose? " + verboseModeEnabled);
-        System.out.println("stdout? " + useStdout);
-        System.out.println("thread #: " + nThreads);
-        System.out.println("remaining size: " + remSize);
-        System.out.println("remaining number of threads: " + remThreads);
+    /**
+     * Log options if verbose mode enabled
+     */
+    private static void logOptions() {
+        if (!verboseModeEnabled)
+            return;
+       logger.info("input file: " + inputFileStr);
+       logger.info("output file: " + outputFileStr);
+       logger.info("batch size: " + batchSize);
+       logger.info("using stdout " + useStdout);
+       logger.info("thread number: " + nThreads);
+       logger.info("remaining work: " + remSize);
+       logger.info("remaining number of threads: " + remThreads);
     }
 
+    // Getters, setters
     public static String getInputFileStr() { return inputFileStr; }
 
     public static String getOutputFileStr() { return outputFileStr; }
