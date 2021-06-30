9c277fa5c3972c02c1d34b76dcef9cfe8a9a1f47
Ege Hurturk <ege.hurturk@gmail.com>
2021-06-30 20:41:27 +0300

    container to store json

--- src/main/java/org/mxretrv/json/JSONDoc.java
+++ src/main/java/org/mxretrv/json/JSONDoc.java
@@ -1,6 +1,30 @@
 package org.mxretrv.json;
 
-public class JSONDoc {
+import java.io.Serializable;
 
+public class JSONDoc implements Serializable {
 
+    /**
+     * JSON content
+     */
+    private String jsonContent;
+
+    public JSONDoc(String jsonContent) {
+        this.jsonContent = jsonContent;
+    }
+
+    public String getJsonContent() {
+        return jsonContent;
+    }
+
+    public void setJsonContent(String jsonContent) {
+        this.jsonContent = jsonContent;
+    }
+
+    @Override
+    public String toString() {
+        return "JSONDoc{" +
+                "jsonContent='" + jsonContent + '\'' +
+                '}';
+    }
 }
