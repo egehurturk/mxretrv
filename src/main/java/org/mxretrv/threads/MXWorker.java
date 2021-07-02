5dbb5e22daed5dd28edc4ed068f9056f96852c4f
Ege Hurturk <ege.hurturk@gmail.com>
2021-07-02 12:34:50 +0300

    Test MXWorker

--- src/main/java/org/mxretrv/threads/MXWorker.java
+++ src/main/java/org/mxretrv/threads/MXWorker.java
@@ -1,4 +1,26 @@
 package org.mxretrv.threads;
 
-public class MXWorker {
+import org.mxretrv.events.Listeners;
+
+public class MXWorker implements Worker, Runnable {
+    private final IOQueue<String> inputQueue;
+    String thing;
+
+    public MXWorker(IOQueue<String> inputQueue, String t) {
+        this.inputQueue = inputQueue;
+        thing = t;
+    }
+
+    @Override
+    public void work() {
+        inputQueue.offer(thing);
+        Listeners.incomingJob();
+    }
+
+    @Override
+    public void run() {
+       work();
+    }
+
+
 }
