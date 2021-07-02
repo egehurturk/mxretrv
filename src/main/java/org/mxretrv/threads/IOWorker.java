c956862e7de703f5189aaed6d0b123675cf9f525
Ege Hurturk <ege.hurturk@gmail.com>
2021-07-02 12:34:46 +0300

    Test IOWorker

--- src/main/java/org/mxretrv/threads/IOWorker.java
+++ src/main/java/org/mxretrv/threads/IOWorker.java
@@ -1,4 +1,23 @@
 package org.mxretrv.threads;
 
-public class IOWorker {
+import org.mxretrv.events.QEventListener;
+
+public class IOWorker implements Worker, QEventListener {
+    private final IOQueue<String> inputQueue;
+    private int level = 0;
+
+    public IOWorker(IOQueue<String> inputQueue) {
+        this.inputQueue = inputQueue;
+    }
+
+    @Override
+    public void work() {
+        System.out.println(inputQueue.poll());
+    }
+
+    @Override
+    public void onEvent() {
+        work();
+    }
 }
+
